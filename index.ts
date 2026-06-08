0dc3baa1ea082cab1d405c51b0ddea7ac4f932bb
elamaruthue <elamaruthue@gmail.com>
2026-06-09 00:49:10 +0530

    cors error fix

--- index.ts
+++ index.ts
@@ -33,7 +33,11 @@ const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 // ─── Express app ──────────────────────────────────────────────────────────────
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174,', 'https://smart-ai-back-end.onrender.com/api' ] }));
+// app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174,', 'https://smart-ai-back-end.onrender.com' ] }));
+app.use(cors({
+  origin: 'https://smart-ai-back-end.onrender.com',
+  credentials: true
+}));
 app.use(express.json());
 
 // ─── Routes ───────────────────────────────────────────────────────────────────
